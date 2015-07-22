747df02a1252afa81e48b9a37ccaf42a429cd572
Will Taylor <will.taylor@tcu.edu>
2015-07-21 21:38:26 -0500

    Added alerts to test gps screenshot

--- scripts.js
+++ scripts.js
@@ -28,9 +28,11 @@ function determineIfMobile(){
 }
 
 function removeGPSScreenshotIfNeeded(){
+  alert("Testing GPS Screenshot")
   var width = window.innerWidth;
 
   if(width <= 826){
+    alert("Removing Screenshot")
     console.log("Removing GPS Screenshot")
     document.getElementById("#removable-gps-screenshot").style.display = "none";
   }
