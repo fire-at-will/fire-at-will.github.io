822386de7f542bc6058d1bddeeb1bf225a0f79d3
fire-at-will <wtaylor151@gmail.com>
2016-06-12 14:49:48 -0500

    Working on workers

--- src/pixelSort.js
+++ src/pixelSort.js
@@ -1,45 +1,231 @@
+var SORT_BY_ROWS = false;
+var SORT_BY_COLUMNS = false;
+var SORT_BY_CIRCLES = false;
+var SORT_INTERVAL = 200;
+var RANDOM_INTERVAL = true;
+
 var imageLoader = document.getElementById('imageLoader');
     imageLoader.addEventListener('change', handleImage, false);
 
-var canvas = document.getElementById('imageCanvas');
+//var canvas = document.getElementById('imageCanvas');
+var canvas = document.createElement('canvas')
 var canvasDiv = document.getElementById('canvasDiv');
-canvas.width = canvasDiv.clientWidth;
-canvas.height = canvasDiv.clientHeight;
-
-$("canvasDiv").hide();
+//canvas.width = canvasDiv.clientWidth;
+//canvas.height = canvasDiv.clientHeight;
 
 var image = new Image();
+var img = new Image();
+var imageData;
 
 var ctx = canvas.getContext('2d');
 
+function drawImage(imageData){
+  ctx.putImageData(imageData, 0, 0);
+  imageItem.src = canvas.toDataURL()
+  //imageItem.style.height = img.height + 'px;'
+}
+
 function handleImage(e){
-    var reader = new FileReader();
-    reader.onload = function(event){
-        var img = new Image();
-        img.onload = function(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
 
-          // Magic image sorcery from stack overflow
-          // http://stackoverflow.com/questions/19262141/resize-image-with-javascript-canvas-smoothly
-          canvas.height = canvas.width * (img.height / img.width);
+    var fileReader = new FileReader();
+    fileReader.onload = function(e) {
+       var img = new Image();
 
-          var oc = document.createElement('canvas'), octx = oc.getContext('2d');
+       img.onload = function() {
 
-          oc.width = img.width * 0.5;
-          oc.height = img.height * 0.5;
-          octx.drawImage(img, 0, 0, oc.width, oc.height);
+            var imageItem = document.getElementById('imageItem');
 
-          octx.drawImage(oc, 0, 0, oc.width * 0.5, oc.height * 0.5);
+            // Set width and height
+            canvas.width = img.width
+            canvas.height = img.height
 
-          ctx.drawImage(oc, 0, 0, oc.width * 0.5, oc.height * 0.5,
-          0, 0, canvas.width, canvas.height);
-        }
-        img.src = event.target.result;
-        image.src = event.target.result;
+            // Draw to canvas
+            ctx.drawImage(img, 0, 0)
 
-    }
-    reader.readAsDataURL(e.target.files[0]);
+            // Put canvas onto img
+            imageItem.src = canvas.toDataURL()
+
+         image = img
+       };
+       img.src = e.target.result;
+
+    };
+    fileReader.readAsDataURL(e.target.files[0] );
 }
 
 function sortImage(){
+  getSettingValues();
 
+  if (window.Worker) {
+
+    var worker = new Worker("src/sort.js");
+
+  }
+
+  // Get image data
+  var tempCanvas = document.createElement('canvas');   // Create new canvas in memory. Otherwise we get the downsized image.
+  var context = tempCanvas.getContext('2d');
+
+  imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  if(SORT_BY_ROWS){
+    console.log("Sorting by rows.")
+
+    for(i = 0; i < imageData.height; i++){
+
+       //console.log("Processing row: " + i + "/" + imageData.height)
+
+      var rowInterval = SORT_INTERVAL
+      if(RANDOM_INTERVAL){
+        // Create a random number x where 2 < x < SORT_INTERVAL
+        rowInterval = Math.floor((Math.random() * SORT_INTERVAL) + 2);
+      }
+
+      var endOfLine = imageData.width
+
+      // How many sections we have to sort in each row
+      var loops = Math.floor(imageData.width / rowInterval)
+      //console.log(loops)
+      for(k = 0; k < loops; k++){
+        var array = []
+
+        // Get the next pixels to sort in the interval
+        for(j = 0; j < rowInterval; j++){
+          //console.log(i)
+          array.push(getPixelData(k * rowInterval + j, i))
+        }
+
+        // Sort this interval
+        quickSort(array)
+        //console.log(array)
+
+        // Replace image pixels with the sorted pixels
+        for(j = 0; j < rowInterval; j++){
+          setPixelData( (k * rowInterval + j), i, array[j] )
+        }
+
+      }
+
+    }
+  }
+
+  // Done sorting!
+  console.log("Done!")
+  drawImage(imageData)
+}
+
+function getSettingValues(){
+
+  // Clear values
+  SORT_BY_ROWS      = false
+  SORT_BY_COLUMNS   = false
+  SORT_BY_CIRCLES   = false
+
+  // Get sort by setting
+  var selectedVal = "";
+  var selected = $("input[type='radio'][name='sort-by-settings']:checked");
+  if (selected.length > 0) {
+      selectedVal = selected.val();
+  }
+  selectedVal = parseInt(selectedVal)
+
+  switch (selectedVal) {
+    case 1:
+      SORT_BY_ROWS = true
+      break;
+    case 2:
+      SORT_BY_COLUMNS = true
+      break;
+    case 3:
+      SORT_BY_CIRCLES = true
+      break;
+    default:
+      break;
+  }
+
+  // Get sort interval
+  SORT_INTERVAL = parseInt(document.getElementById('sortIntervalInput').value)
+
+  // Get RANDOM_INTERVAL boolean
+  RANDOM_INTERVAL = document.getElementById('random-interval-switch').checked
+}
+
+function quickSort(pixelArray){
+  quickSortAlgorithm(pixelArray, 0, pixelArray.length - 1)
+}
+
+function quickSortAlgorithm(pixelArray, low, high){
+  if(low < high){
+    let midpoint = partition(pixelArray, low, high)
+
+    quickSortAlgorithm(pixelArray, low, midpoint - 1)
+    quickSortAlgorithm(pixelArray, midpoint + 1, high)
+  }
+
+}
+
+function partition(pixelArray, low, high){
+
+  let pivot = getPixelValue(pixelArray[low])
+
+  var leftmark = low + 1
+  var rightmark = high
+
+  var done = false
+
+  while(!done){
+
+    while(leftmark <= rightmark && ( getPixelValue(pixelArray[leftmark]) <= pivot ) ){
+      leftmark = leftmark + 1
+    }
+
+
+    while( (getPixelValue(pixelArray[rightmark]) >= pivot ) && rightmark >= leftmark ){
+      rightmark = rightmark - 1
+    }
+
+    if(rightmark < leftmark){
+      done = true
+    } else {
+      let temp = pixelArray[leftmark]
+      pixelArray[leftmark] = pixelArray[rightmark]
+      pixelArray[rightmark] = temp
+    }
+
+  }
+
+  let temp = pixelArray[low]
+  pixelArray[low] = pixelArray[rightmark]
+  pixelArray[rightmark] = temp
+
+  return rightmark
+}
+
+function setPixelData(x, y, dataArray){
+  // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Pixel_manipulation_with_canvas
+
+  imageData.data[((y * (imageData.width * 4)) + (x * 4)) + 0] = dataArray[0]
+  imageData.data[((y * (imageData.width * 4)) + (x * 4)) + 1] = dataArray[1]
+  imageData.data[((y * (imageData.width * 4)) + (x * 4)) + 2] = dataArray[2]
+}
+
+function getPixelData(x, y){
+  // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Pixel_manipulation_with_canvas
+
+  let r = imageData.data[((y * (imageData.width * 4)) + (x * 4)) + 0];
+  let g = imageData.data[((y * (imageData.width * 4)) + (x * 4)) + 1];
+  let b = imageData.data[((y * (imageData.width * 4)) + (x * 4)) + 2];
+
+
+  return [r, g, b]
+}
+
+function getPixelValue(pixelData){
+  // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Pixel_manipulation_with_canvas
+
+  let r = pixelData[0]
+  let g = pixelData[1]
+  let b = pixelData[2]
+  return r + g + b
 }
