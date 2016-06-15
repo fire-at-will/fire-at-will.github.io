7eba5c175951b2afcb7896c86309a0d0e4e0a5f8
fire-at-will <wtaylor151@gmail.com>
2016-06-14 20:30:46 -0500

    Will an empty string make that error go away?

--- src/pixelSort.js
+++ src/pixelSort.js
@@ -55,7 +55,7 @@ function handleImage(e){
           sortButton.removeAttribute("disabled")
           image = img
           let inputButton = document.getElementById('imageLoader');
-          inputButton.value = undefined;
+          inputButton.value = "";
        };
        img.src = e.target.result;
 
